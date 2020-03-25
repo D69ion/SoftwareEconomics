a0606b4a62a492d6303070e79aeaca92b87f61e7
Дмитрий Штеле <dmitriy.shtele@gmail.com>
2020-03-25 15:26:17 +0400

    lab complete

--- SoftwareEconomics/Form1.Designer.cs
+++ SoftwareEconomics/Form1.Designer.cs
@@ -39,19 +39,30 @@
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView4 = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView6 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -146,15 +157,35 @@
             this.Column6});
             this.dataGridView3.Location = new System.Drawing.Point(300, 136);
             this.dataGridView3.Name = "dataGridView3";
-            this.dataGridView3.Size = new System.Drawing.Size(444, 150);
+            this.dataGridView3.Size = new System.Drawing.Size(444, 126);
             this.dataGridView3.TabIndex = 4;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "Этапы жизненного цикла";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Column5.HeaderText = "Численность Zi";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 102;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Column6.HeaderText = "Длительность Дi";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 109;
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 269);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(269, 169);
+            this.textBox1.Size = new System.Drawing.Size(269, 150);
             this.textBox1.TabIndex = 5;
             // 
             // dataGridView4
@@ -165,7 +196,7 @@
             this.dataGridViewTextBoxColumn2,
             this.dataGridViewTextBoxColumn3,
             this.Column7});
-            this.dataGridView4.Location = new System.Drawing.Point(300, 292);
+            this.dataGridView4.Location = new System.Drawing.Point(300, 269);
             this.dataGridView4.Name = "dataGridView4";
             this.dataGridView4.Size = new System.Drawing.Size(444, 150);
             this.dataGridView4.TabIndex = 6;
@@ -197,31 +228,83 @@
             this.Column7.Name = "Column7";
             this.Column7.Width = 113;
             // 
-            // Column4
+            // dataGridView5
             // 
-            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column4.HeaderText = "Этапы жизненного цикла";
-            this.Column4.Name = "Column4";
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.Column8});
+            this.dataGridView5.Location = new System.Drawing.Point(12, 425);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.Size = new System.Drawing.Size(536, 146);
+            this.dataGridView5.TabIndex = 7;
             // 
-            // Column5
+            // dataGridViewTextBoxColumn4
             // 
-            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
-            this.Column5.HeaderText = "Численность Zi";
-            this.Column5.Name = "Column5";
-            this.Column5.Width = 102;
+            this.dataGridViewTextBoxColumn4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn4.HeaderText = "Этапы жизненного цикла";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
             // 
-            // Column6
+            // dataGridViewTextBoxColumn5
             // 
-            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
-            this.Column6.HeaderText = "Длительность Дi";
-            this.Column6.Name = "Column6";
-            this.Column6.Width = 109;
+            this.dataGridViewTextBoxColumn5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.dataGridViewTextBoxColumn5.HeaderText = "Аналитик";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.Width = 80;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.dataGridViewTextBoxColumn6.HeaderText = "Программист";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.Width = 102;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.dataGridViewTextBoxColumn7.HeaderText = "Техник";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.Width = 68;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "ФЗП по этапу";
+            this.Column8.Name = "Column8";
+            // 
+            // dataGridView6
+            // 
+            this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView6.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn8,
+            this.dataGridViewTextBoxColumn9});
+            this.dataGridView6.Location = new System.Drawing.Point(751, 41);
+            this.dataGridView6.Name = "dataGridView6";
+            this.dataGridView6.Size = new System.Drawing.Size(368, 267);
+            this.dataGridView6.TabIndex = 8;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn8.HeaderText = "Наименование статей расходов";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.dataGridViewTextBoxColumn9.HeaderText = "Сумма";
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            this.dataGridViewTextBoxColumn9.Width = 66;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1130, 655);
+            this.Controls.Add(this.dataGridView6);
+            this.Controls.Add(this.dataGridView5);
             this.Controls.Add(this.dataGridView4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView3);
@@ -235,6 +318,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -262,6 +347,15 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridView dataGridView6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
     }
 }
 
--- SoftwareEconomics/Form1.cs
+++ SoftwareEconomics/Form1.cs
@@ -43,32 +43,93 @@ namespace SoftwareEconomics
                 case 2:
                     textBox1.Text += "методом функциональных точек" + Environment.NewLine;
                     break;
-
             }
             int T = Convert.ToInt32(dataGridView2.Rows[min].Cells[1].Value),
                 Z = Convert.ToInt32(dataGridView2.Rows[min].Cells[3].Value),
                 D = Convert.ToInt32(dataGridView1.Rows[1].Cells[1].Value);
-            int SalaryFund = CalcSalaryFund(T, Z, D);
+            CalcSalaryFund(T, Z, D);
+
+            int Zop = (int)Math.Floor(Convert.ToInt32(dataGridView1.Rows[1].Cells[1].Value) / 2 * 0.0095);
+            int Sop = (int)Math.Round(Zop * Convert.ToInt32(dataGridView1.Rows[1].Cells[1].Value) / 2 * Convert.ToInt32(dataGridView1.Rows[8].Cells[1].Value) * 0.85);
+            int S = Sop + Convert.ToInt32(dataGridView5.Rows[4].Cells[4].Value);
+            FillFinalTable(S);
         }
 
-        private int CalcSalaryFund(int T, int Z, int D)
+        private void CalcSalaryFund(int T, int Z, int D)
         {
-            //табл 1.7
-            dataGridView3.RowCount = 4;
-            dataGridView3.Rows[0].Cells[0].Value = "Анализ предметной области и разработка требований";
-            dataGridView3.Rows[1].Cells[0].Value = "Проектирование";
-            dataGridView3.Rows[2].Cells[0].Value = "Программирование";
-            dataGridView3.Rows[3].Cells[0].Value = "Тестирование и комплексные испытания";
-            dataGridView3.Rows[0].Cells[1].Value = Math.Floor((0.1 * T) / (0.1 * D));
-            dataGridView3.Rows[1].Cells[1].Value = Math.Floor((0.22 * T) / (0.3 * D));
-            dataGridView3.Rows[2].Cells[1].Value = Math.Floor((0.405 * T) / (0.35 * D));
-            dataGridView3.Rows[3].Cells[1].Value = Math.Floor((0.275 * T) / (0.25 * D));
-            dataGridView3.Rows[0].Cells[2].Value = Math.Floor(0.1 * D);
-            dataGridView3.Rows[1].Cells[2].Value = Math.Floor(0.3 * D);
-            dataGridView3.Rows[2].Cells[2].Value = Math.Floor(0.35 * D);
-            dataGridView3.Rows[3].Cells[2].Value = Math.Floor(0.25 * D);
+            FillTable17(T, D);
+            FillTable19();
+            FillTable110();
+        }
 
-            //проблемы в кол-ве людейю они бывают 0
+        private void FillFinalTable(int S)
+        {
+            //таб 1.11
+            dataGridView6.RowCount = 11;
+            dataGridView6.Rows[0].Cells[0].Value = "Фонд оплаты труда";
+            dataGridView6.Rows[1].Cells[0].Value = "Страховые взносы в ПФР, ФСС, и ФОМС";
+            dataGridView6.Rows[2].Cells[0].Value = "Увеличение стоимости основных средств";
+            dataGridView6.Rows[3].Cells[0].Value = "Коммунальные услуги, услуги связи";
+            dataGridView6.Rows[4].Cells[0].Value = "Прочие расходы";
+            dataGridView6.Rows[5].Cells[0].Value = "Итого прямые затраты";
+            dataGridView6.Rows[6].Cells[0].Value = "Фонд развития производства";
+            dataGridView6.Rows[7].Cells[0].Value = "Накладные расходы";
+            dataGridView6.Rows[8].Cells[0].Value = "Всего расходов";
+            dataGridView6.Rows[9].Cells[0].Value = "Налог на добавленную стоимость";
+            dataGridView6.Rows[10].Cells[0].Value = "ИТОГО ДОГОВОРНАЯ ЦЕНА";
+
+            dataGridView6.Rows[0].Cells[1].Value = S;
+            dataGridView6.Rows[1].Cells[1].Value = Math.Round(S * 0.3, 2);
+            dataGridView6.Rows[2].Cells[1].Value = 0;//уточнить
+            dataGridView6.Rows[3].Cells[1].Value = 1000 * Convert.ToInt32(dataGridView1.Rows[1].Cells[1].Value);
+            dataGridView6.Rows[4].Cells[1].Value = 500 * Convert.ToInt32(dataGridView1.Rows[1].Cells[1].Value);
+            dataGridView6.Rows[5].Cells[1].Value = Convert.ToInt32(dataGridView6.Rows[0].Cells[1].Value) + Convert.ToInt32(dataGridView6.Rows[1].Cells[1].Value) +
+                Convert.ToInt32(dataGridView6.Rows[2].Cells[1].Value) + Convert.ToInt32(dataGridView6.Rows[3].Cells[1].Value) +
+                Convert.ToInt32(dataGridView6.Rows[4].Cells[1].Value);
+            dataGridView6.Rows[6].Cells[1].Value = Math.Round(Convert.ToInt32(dataGridView6.Rows[5].Cells[1].Value) * 0.1, 2);
+            dataGridView6.Rows[7].Cells[1].Value = Math.Round(Convert.ToInt32(dataGridView6.Rows[5].Cells[1].Value) * 0.12, 2);
+            dataGridView6.Rows[8].Cells[1].Value = Math.Round(Convert.ToDouble(dataGridView6.Rows[5].Cells[1].Value) + Convert.ToDouble(dataGridView6.Rows[6].Cells[1].Value) +
+                Convert.ToDouble(dataGridView6.Rows[7].Cells[1].Value), 2);
+            dataGridView6.Rows[9].Cells[1].Value = Math.Round(Convert.ToDouble(dataGridView6.Rows[8].Cells[1].Value) * 0.18, 2);
+            dataGridView6.Rows[10].Cells[1].Value = Math.Round(Convert.ToDouble(dataGridView6.Rows[8].Cells[1].Value) + Convert.ToDouble(dataGridView6.Rows[9].Cells[1].Value), 2);
+
+        }
+
+        private void FillTable110()
+        {
+            //табл 1.10
+            dataGridView5.RowCount = 5;
+            dataGridView5.Rows[0].Cells[0].Value = "Анализ предметной области и разработка требований";
+            dataGridView5.Rows[1].Cells[0].Value = "Проектирование";
+            dataGridView5.Rows[2].Cells[0].Value = "Программирование";
+            dataGridView5.Rows[3].Cells[0].Value = "Тестирование и комплексные испытания";
+            dataGridView5.Rows[4].Cells[0].Value = "Итого фонд заработной платы";
+
+            int[] salaries = new int[4];
+            salaries[1] = (int)Math.Round(Convert.ToInt32(dataGridView1.Rows[8].Cells[1].Value) * 1.3);
+            salaries[2] = Convert.ToInt32(dataGridView1.Rows[8].Cells[1].Value);
+            salaries[3] = (int)Math.Round(Convert.ToInt32(dataGridView1.Rows[8].Cells[1].Value) * 0.7);
+
+            for (int i = 1; i < 4; i++)
+            {
+                dataGridView5.Rows[0].Cells[i].Value = Convert.ToInt32(dataGridView4.Rows[0].Cells[i].Value) * Convert.ToInt32(dataGridView3.Rows[0].Cells[2].Value) * salaries[i];
+                dataGridView5.Rows[1].Cells[i].Value = Convert.ToInt32(dataGridView4.Rows[1].Cells[i].Value) * Convert.ToInt32(dataGridView3.Rows[1].Cells[2].Value) * salaries[i];
+                dataGridView5.Rows[2].Cells[i].Value = Convert.ToInt32(dataGridView4.Rows[2].Cells[i].Value) * Convert.ToInt32(dataGridView3.Rows[2].Cells[2].Value) * salaries[i];
+                dataGridView5.Rows[3].Cells[i].Value = Convert.ToInt32(dataGridView4.Rows[3].Cells[i].Value) * Convert.ToInt32(dataGridView3.Rows[3].Cells[2].Value) * salaries[i];
+            }
+
+            int sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                dataGridView5.Rows[i].Cells[4].Value = Convert.ToInt32(dataGridView5.Rows[i].Cells[1].Value) + Convert.ToInt32(dataGridView5.Rows[i].Cells[2].Value) +
+                    Convert.ToInt32(dataGridView5.Rows[i].Cells[3].Value);
+                sum += Convert.ToInt32(dataGridView5.Rows[i].Cells[4].Value);
+            }
+            dataGridView5.Rows[4].Cells[4].Value = sum;
+        }
+
+        private void FillTable19()
+        {
             //табл 1.9
             dataGridView4.RowCount = 4;
             dataGridView4.Rows[0].Cells[0].Value = "Анализ предметной области и разработка требований";
@@ -90,13 +151,29 @@ namespace SoftwareEconomics
             dataGridView4.Rows[1].Cells[3].Value = Math.Floor(Convert.ToInt32(dataGridView3.Rows[1].Cells[1].Value) * 0.3);
             dataGridView4.Rows[2].Cells[3].Value = Math.Floor(Convert.ToInt32(dataGridView3.Rows[2].Cells[1].Value) * 0.25);
             dataGridView4.Rows[3].Cells[3].Value = Math.Floor(Convert.ToInt32(dataGridView3.Rows[3].Cells[1].Value) * 0.25);
+        }
 
-            return 0;
+        private void FillTable17(int T, int D)
+        {
+            //табл 1.7
+            dataGridView3.RowCount = 4;
+            dataGridView3.Rows[0].Cells[0].Value = "Анализ предметной области и разработка требований";
+            dataGridView3.Rows[1].Cells[0].Value = "Проектирование";
+            dataGridView3.Rows[2].Cells[0].Value = "Программирование";
+            dataGridView3.Rows[3].Cells[0].Value = "Тестирование и комплексные испытания";
+            dataGridView3.Rows[0].Cells[2].Value = Math.Floor(0.1 * D);
+            dataGridView3.Rows[1].Cells[2].Value = Math.Floor(0.3 * D);
+            dataGridView3.Rows[2].Cells[2].Value = Math.Floor(0.35 * D);
+            dataGridView3.Rows[3].Cells[2].Value = Math.Floor(0.25 * D);
+            dataGridView3.Rows[0].Cells[1].Value = Math.Floor((0.1 * T) / Convert.ToInt32(dataGridView3.Rows[0].Cells[2].Value));
+            dataGridView3.Rows[1].Cells[1].Value = Math.Floor((0.22 * T) / Convert.ToInt32(dataGridView3.Rows[1].Cells[2].Value));
+            dataGridView3.Rows[2].Cells[1].Value = Math.Floor((0.405 * T) / Convert.ToInt32(dataGridView3.Rows[2].Cells[2].Value));
+            dataGridView3.Rows[3].Cells[1].Value = Math.Floor((0.275 * T) / Convert.ToInt32(dataGridView3.Rows[3].Cells[2].Value));
         }
 
         private int FirstMethod()
         {
-            //P - 220 строк/чел-месяц
+            //P - 220 строк/чел-месяц ибо Б30к строк и ИСС
             double P = 220;
             double R = Convert.ToInt32(dataGridView1.Rows[2].Cells[1].Value);
             double T = R / P;
@@ -104,7 +181,7 @@ namespace SoftwareEconomics
             dataGridView2.Rows[0].Cells[1].Value = Math.Floor(T);
             dataGridView2.Rows[0].Cells[2].Value = dataGridView1.Rows[1].Cells[1].Value;
             dataGridView2.Rows[0].Cells[3].Value = Math.Floor(Z);
-            return (int)Math.Floor(T) * (int)Math.Floor(Z);
+            return Convert.ToInt32(dataGridView2.Rows[0].Cells[1].Value) * Convert.ToInt32(dataGridView2.Rows[0].Cells[3].Value);
         }
 
         private int SecondMethod()
@@ -122,7 +199,7 @@ namespace SoftwareEconomics
             dataGridView2.Rows[1].Cells[1].Value = Math.Floor(T);
             dataGridView2.Rows[1].Cells[2].Value = dataGridView1.Rows[1].Cells[1].Value;
             dataGridView2.Rows[1].Cells[3].Value = Math.Floor(Z);
-            return (int)Math.Floor(T) * (int)Math.Floor(Z);
+            return Convert.ToInt32(dataGridView2.Rows[1].Cells[1].Value) * Convert.ToInt32(dataGridView2.Rows[1].Cells[3].Value);
         }
 
         private int ThirdMethod()
@@ -184,12 +261,12 @@ namespace SoftwareEconomics
             double Rloc = RF * LOC;
             double A = 3;
             double E = 1.12;
-            double T = A * Math.Pow(Rloc / 1000, E) / 12;
+            double T = A * Math.Pow((int)(Rloc / 1000), E) / 12;
             double Z = T / Convert.ToInt32(dataGridView1.Rows[1].Cells[1].Value);
             dataGridView2.Rows[2].Cells[1].Value = Math.Floor(T);
             dataGridView2.Rows[2].Cells[2].Value = dataGridView1.Rows[1].Cells[1].Value;
             dataGridView2.Rows[2].Cells[3].Value = Math.Floor(Z);
-            return (int)Math.Floor(T) * (int)Math.Floor(Z);
+            return Convert.ToInt32(dataGridView2.Rows[2].Cells[1].Value) * Convert.ToInt32(dataGridView2.Rows[2].Cells[3].Value);
         }
 
         private void FillInitialValues()
