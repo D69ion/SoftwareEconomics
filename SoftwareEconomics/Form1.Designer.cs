ea2a1f67d4419d07549fb617abb6d2488b584602
Дмитрий Штеле <dmitriy.shtele@gmail.com>
2020-04-12 20:12:35 +0400

    1

--- SoftwareEconomics/Form1.Designer.cs
+++ SoftwareEconomics/Form1.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -42,10 +45,12 @@
             this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -111,7 +116,7 @@
             this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn3,
             this.dataGridViewTextBoxColumn4});
-            this.dataGridView3.Location = new System.Drawing.Point(320, 158);
+            this.dataGridView3.Location = new System.Drawing.Point(628, 19);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.Size = new System.Drawing.Size(302, 133);
             this.dataGridView3.TabIndex = 3;
@@ -163,11 +168,28 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(320, 158);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(610, 280);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1007, 450);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView4);
             this.Controls.Add(this.dataGridView3);
@@ -180,6 +202,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,6 +224,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- SoftwareEconomics/Form1.cs
+++ SoftwareEconomics/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace SoftwareEconomics
 {
@@ -25,10 +26,11 @@ namespace SoftwareEconomics
             FillTable22(s);
             int a = Convert.ToInt32(dataGridView3.Rows[4].Cells[1].Value),
                 b = Convert.ToInt32(dataGridView4.Rows[4].Cells[1].Value);
-            double x0 = Math.Ceiling(a / (s - b));
+            double x0 = a / (s - b);
             textBox1.Text += string.Format("В течение месяца фирме необходимо подготовить и продать миниммум {0} копий " +
                 "программного продукта по цене {1} руб., чтобы окупить постоянные и переменные расходыв рамках " +
-                "ее деятельности на создание программного продукта" + Environment.NewLine + Environment.NewLine, x0, s);
+                "ее деятельности на создание программного продукта" + Environment.NewLine + Environment.NewLine, Math.Ceiling(x0), s);
+            FillGraph(x0, s);
 
             int xp = Convert.ToInt32(dataGridView2.Rows[1].Cells[1].Value);
             double sm = Math.Round((double)(a + b * xp) / xp, 2);
@@ -48,7 +50,56 @@ namespace SoftwareEconomics
             textBox1.Text += string.Format("Срок окупаемости проекта при продаже не менее {0} копий продукта в месяц " +
                 "(точка безубыточности) и рыночной стоимости {1} руб. за копию составит {2} месяцев." + Environment.NewLine +
                 "Для того чтобы окупить все расходы на реализацию проекта, необходимо продать {3} копий программного продукта" +
-                Environment.NewLine, x0, s, Cok, xn);
+                Environment.NewLine, Math.Ceiling(x0), s, Cok, xn);
+        }
+
+        private void FillGraph(double x0, double s)
+        {
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+
+            chart1.ChartAreas[0].AxisY.LabelAutoFitMaxFontSize = 8;
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = 15;
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.ChartAreas[0].Position.Width = 66;
+            chart1.Series.Clear();
+
+            Series series = new Series();
+            series.LegendText = "Постоянные издержки";
+            series.ChartType = SeriesChartType.Line;
+            series.Points.AddXY(0, Convert.ToInt32(dataGridView3.Rows[4].Cells[1].Value));
+            series.Points.AddXY(x0, Convert.ToInt32(dataGridView3.Rows[4].Cells[1].Value));
+            chart1.Series.Add(series);
+            
+            series = new Series();
+            series.LegendText = "Переменные издержки";
+            series.ChartType = SeriesChartType.Line;
+            series.Points.AddXY(0, 0);
+            series.Points.AddXY(x0, Convert.ToInt32(dataGridView4.Rows[4].Cells[1].Value));
+            chart1.Series.Add(series);
+
+            series = new Series();
+            series.LegendText = "Общие издержки";
+            series.ChartType = SeriesChartType.Line;
+            series.Points.AddXY(0, Convert.ToInt32(dataGridView3.Rows[4].Cells[1].Value));
+            series.Points.AddXY(x0, Convert.ToInt32(dataGridView3.Rows[4].Cells[1].Value) + Convert.ToInt32(dataGridView4.Rows[4].Cells[1].Value));
+            chart1.Series.Add(series);
+
+            series = new Series();
+            series.LegendText = "Точка безубыточности";
+            series.ChartType = SeriesChartType.Line;
+            series.Points.AddXY(x0, 0);
+            series.Points.AddXY(x0, Convert.ToInt32(dataGridView3.Rows[4].Cells[1].Value) + Convert.ToInt32(dataGridView4.Rows[4].Cells[1].Value));
+            chart1.Series.Add(series);
+
+            series = new Series();
+            series.LegendText = "Доход от продаж";
+            series.ChartType = SeriesChartType.Line;
+            series.Points.AddXY(0, 0);
+            series.Points.AddXY(x0, x0 * s - Convert.ToInt32(dataGridView3.Rows[4].Cells[1].Value) - Convert.ToInt32(dataGridView4.Rows[4].Cells[1].Value));
+            chart1.Series.Add(series);
+
         }
 
         private void FillTable21()
@@ -57,9 +108,9 @@ namespace SoftwareEconomics
             dataGridView3.Rows[0].Cells[0].Value = "Накладные расходы по проекту";
             dataGridView3.Rows[0].Cells[1].Value = 10000;
             dataGridView3.Rows[1].Cells[0].Value = "Плановое ежемесячное гашение кредита";
-            dataGridView3.Rows[1].Cells[1].Value = Math.Round(Convert.ToDouble(dataGridView1.Rows[0].Cells[1].Value) * Convert.ToDouble(dataGridView2.Rows[0].Cells[1].Value) / 100, 2);
+            dataGridView3.Rows[1].Cells[1].Value = Math.Round(Convert.ToDouble(dataGridView1.Rows[0].Cells[1].Value) * Convert.ToDouble(dataGridView2.Rows[0].Cells[1].Value) / 100 / 12, 2);
             dataGridView3.Rows[2].Cells[0].Value = "Выплата среднего банковского процента";
-            dataGridView3.Rows[2].Cells[1].Value = 0;
+            dataGridView3.Rows[2].Cells[1].Value = Math.Round(Convert.ToDouble(dataGridView1.Rows[0].Cells[1].Value) * Convert.ToDouble(dataGridView2.Rows[0].Cells[1].Value) / 100, 2);
             dataGridView3.Rows[3].Cells[0].Value = "Прочие расходы";
             dataGridView3.Rows[3].Cells[1].Value = 1000;
             dataGridView3.Rows[4].Cells[0].Value = "Итого";
@@ -88,7 +139,7 @@ namespace SoftwareEconomics
         {
             dataGridView1.RowCount = 3;
             dataGridView1.Rows[0].Cells[0].Value = "Размер кредита";
-            dataGridView1.Rows[0].Cells[1].Value = 1194206;
+            dataGridView1.Rows[0].Cells[1].Value = 1280582;
             dataGridView1.Rows[1].Cells[0].Value = "Срок кредита";
             dataGridView1.Rows[1].Cells[1].Value = 15;
             dataGridView1.Rows[2].Cells[0].Value = "% годовых";
